7bc6d70708d4b4d847eb6d7f51e01bdc8e267cc7
purusharma <psharma@datopic.com>
2024-09-24 15:43:49 +0530

    updated code

--- app.py
+++ app.py
@@ -1,24 +1,42 @@
-# Python program to split array and move first
-# part to end.
- 
- 
-def splitArr(arr, n, k):
-    for i in range(0, k):
-        x = arr[0]
-        for j in range(0, n-1):
-            arr[j] = arr[j + 1]
- 
-        arr[n-1] = x
- 
- 
-# main
-arr = [12, 10, 5, 6, 52, 36]
-n = len(arr)
-position = 2
- 
-splitArr(arr, n, position)
- 
-for i in range(0, n):
-    print(arr[i], end=' ')
- 
-# Code Contributed by Mohit Gupta_OMG <(0_o)>
+def compress_string(input_string):
+    if not input_string:
+        return ""
+
+    compressed = []
+    count = 1
+
+    for i in range(1, len(input_string)):
+        if input_string[i] == input_string[i - 1]:
+            count += 1
+        else:
+            compressed.append(f"{input_string[i - 1]}{count}")
+            count = 1
+
+    compressed.append(f"{input_string[-1]}{count}")
+    return ''.join(compressed)
+
+input_string = "ccaaaabbzzx"
+result = compress_string(input_string)
+print(result)
+
+def my_decorator(func):
+    def wrapper():
+        print("something is happening before function called ")
+        func()
+        print("something is happening after function called")
+
+    return wrapper
+
+
+@my_decorator
+def say_hello():
+    print("hello")
+
+
+say_hello()
+
+nums = [1,2,3,4,5]
+result = ["Even" if x % 2 == 0 else "Odd" for x in nums]
+print(result)
+
+
